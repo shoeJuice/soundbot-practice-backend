fe223762877e57faa45ae5dcfe9e9565836f7340
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-07-23 13:28:52 -0400

    Changed comand for procfile, changed port for defaults

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 80,
     host: process.env.HOST || 'localhost',
     corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:3000',
 }
